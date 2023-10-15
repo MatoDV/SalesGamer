c7c0d6fbf81ab4ec0e89fe61ca5aa76a995eb018
Matias Brizuela <115161150+MatoDV@users.noreply.github.com>
2023-10-14 21:20:23 -0300

    feat: fixing

--- EjemploABM/Modelo/Producto.cs
+++ EjemploABM/Modelo/Producto.cs
@@ -13,18 +13,16 @@ namespace EjemploABM.Modelo
         public String Nombre { get; set; }
         public String Descripcion { get; set; }
         public Categoria Cat { get; set; }
-        public String Imagen { get; set; }
         public int Precio { get; set; }
         public int Stock { get; set; }
 
 
-        public Producto(int id, string name, string desc, Categoria cat, string img, int precio, int stock)
+        public Producto(int id, string name, string desc, Categoria cat, int precio, int stock)
         {
             Id = id;
             Nombre = name;
             Descripcion = desc;
             Cat = cat;
-            Imagen = img;
             Precio = precio;
             Stock = stock;
         }
